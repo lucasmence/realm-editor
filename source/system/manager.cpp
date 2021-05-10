1cba40ef0c1142bee854ce8079f20e5e611d80c7
lucasmence <lucas21021996@gmail.com>
2021-05-10 11:32:45 -0300

    Updated manager due new classes; #1

--- source/system/manager.cpp
+++ source/system/manager.cpp
@@ -2,12 +2,62 @@
 
 Manager::Manager()
 {
-	this->window = std::shared_ptr<sf::RenderWindow>(new sf::RenderWindow(sf::VideoMode(1380, 860), "realm-editor", sf::Style::Default));
+    this->unloadAll();
+
+	this->window = std::make_shared<sf::RenderWindow>(sf::VideoMode(1380, 860), "realm-editor", sf::Style::Default);
+
+    this->font = std::shared_ptr<sf::Font>(new sf::Font);
+    this->font->loadFromFile("resources/fonts/consola.ttf");
+
+    this->addView(ObjectType::otModel, std::static_pointer_cast<ViewElement>(std::make_shared<Model>(this, "textures/terrain/castle-cobblestone")));
+    this->addView(ObjectType::otText, std::static_pointer_cast<ViewElement>(std::make_shared<Text>(this, "Hello world", 20, sf::Vector2f(50.f, 50.f))));
 }
 
 Manager::~Manager()
 {
+    this->unloadAll();
+}
 
+bool Manager::addView(ObjectType type, std::shared_ptr<ViewElement> element)
+{
+    switch (type)
+    {
+        case (ObjectType::otModel):
+        {
+            this->list.models.emplace_back(element);
+            this->list.viewElements.emplace_back(element);
+            break;
+        }
+        case (ObjectType::otText):
+        {
+            this->list.texts.emplace_back(element);
+            this->list.viewElements.emplace_back(element);
+            break;
+        }
+    }
+
+    return true;
+}
+
+bool Manager::removeView(ObjectType type, std::shared_ptr<ViewElement> element)
+{
+    switch (type)
+    {
+        case (ObjectType::otModel):
+        {
+            this->list.models.remove(element);
+            this->list.viewElements.remove(element);
+            break;
+        }
+        case (ObjectType::otText):
+        {
+            this->list.texts.remove(element);
+            this->list.viewElements.remove(element);
+            break;
+        }
+    }
+
+    return true;
 }
 
 bool Manager::update()
@@ -15,6 +65,10 @@ bool Manager::update()
     this->event();
 
     this->window->clear();
+
+    for (auto& element : this->list.viewElements)
+        element->draw();
+
     this->window->display();
 
 	return true;
@@ -51,5 +105,9 @@ bool Manager::eventClick(sf::Event& event)
 
 bool Manager::unloadAll()
 {
+    this->list.textures.clear();
+    this->list.models.clear();
+    this->list.texts.clear();
+    this->list.viewElements.clear();
 	return true;
 }
--- source/system/manager.hpp
+++ source/system/manager.hpp
@@ -3,21 +3,32 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include "text/text.hpp"
+#include "model/model.hpp"
+#include "text/text.hpp"
+#include "object/viewElement.hpp"
 
 #pragma once
 
 #ifndef MANAGER_HPP
 #define MANAGER_HPP
 
+enum class ObjectType {otModel, otText};
+
 struct ManagerList
 {
-
+	std::list<std::shared_ptr<ViewElement>> viewElements;
+	std::list<std::shared_ptr<ViewElement>> models;
+	std::list<std::shared_ptr<ViewElement>> texts;
+	std::list<std::shared_ptr<Texture>> textures;
 };
 
 class Manager
 {
 	public:
 		std::shared_ptr<sf::RenderWindow> window;
+		std::shared_ptr<sf::Font> font;
+		ManagerList list;
 
 		Manager();
 		~Manager();
@@ -26,6 +37,8 @@ class Manager
 		bool update();
 		bool event();
 		bool eventClick(sf::Event& event);
+		bool addView(ObjectType type, std::shared_ptr<ViewElement> element);
+		bool removeView(ObjectType type, std::shared_ptr<ViewElement> element);
 };
 
 #endif
