e7e6c3026105ba6a5f06ad0341fd2635bb6bff6a
lucasmence <lucas21021996@gmail.com>
2021-05-13 14:35:00 -0300

    Improved class constructor and behavior; #1

--- source/system/hud/button.cpp
+++ source/system/hud/button.cpp
@@ -1,17 +1,45 @@
 #include "button.hpp"
 #include "../manager.hpp"
+#include "../library/position.hpp"
 
-Button::Button(Manager* manager, std::string caption, sf::Vector2f position, int size)
+Button::Button(Manager* manager, 
+			   std::string caption, 
+			   sf::Vector2f position, 
+			   std::string name, 
+			   int size,
+			   std::shared_ptr<Button> neighbor,
+			   sf::Vector2i side)
 {
-	this->text = std::make_shared<Text>(manager, caption, size, position);
+	this->selected = false;
+	this->manager = manager;
+	this->name = name;
+	this->label = std::make_shared<Label>(manager, caption, size, position);
 	this->shape = std::make_shared<Model>(manager, sf::Vector2f(position.x - 5.f, position.y));
-	this->shape->loadShape(sf::Vector2f(this->text->text->getGlobalBounds().width + 10.f, this->text->text->getGlobalBounds().height + 10.f), sf::Color(150, 150, 150, 100));
+	this->shape->loadShape(sf::Vector2f(this->label->text->getGlobalBounds().width + 10.f, size * 1.25f), sf::Color(150, 150, 150, 100));
 
-	manager->addView(ObjectType::otText, std::static_pointer_cast<ViewElement>(this->text));
-	manager->addView(ObjectType::otModel, std::static_pointer_cast<ViewElement>(this->shape));
+	if (neighbor)
+	{
+		sf::Vector2f positionSide = position::getSidePosition(neighbor->shape->shape->getGlobalBounds(), this->shape->shape->getGlobalBounds(), position, side);
+
+		this->shape->shape->setPosition(positionSide);
+		this->label->text->setPosition(sf::Vector2f(positionSide.x + 5.f, positionSide.y));
+			
+	}
+
+	manager->addView(std::static_pointer_cast<ViewElement>(this->label));
+	manager->addView(std::static_pointer_cast<ViewElement>(this->shape));
 }
 
 Button::~Button()
 {
+	this->clear();
+}
 
+bool Button::clear()
+{
+	this->manager->removeView(std::static_pointer_cast<ViewElement>(this->label));
+	this->manager->removeView(std::static_pointer_cast<ViewElement>(this->shape));
+	this->label = nullptr;
+	this->shape = nullptr;
+	return true;
 }
--- source/system/hud/button.hpp
+++ source/system/hud/button.hpp
@@ -1,7 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <memory>
 #include "../model/model.hpp"
-#include "../text/text.hpp"
+#include "../text/label.hpp"
 
 #pragma once
 
@@ -13,11 +13,21 @@ class Manager;
 class Button 
 {
 	public:
+		Manager* manager;
 		std::shared_ptr<Model> shape;
-		std::shared_ptr<Text> text;
+		std::shared_ptr<Label> label;
+		std::string name;
+		bool selected;
 
-		Button(Manager* manager, std::string caption, sf::Vector2f position, int size = 20);
+		Button(Manager* manager, 
+			   std::string caption, 
+			   sf::Vector2f position, 
+			   std::string name, 
+			   int size = 20, 
+			   std::shared_ptr<Button> neighbor = nullptr, 
+			   sf::Vector2i side = sf::Vector2i(0, 0));
 		~Button();
+		bool clear();
 
 };
 
