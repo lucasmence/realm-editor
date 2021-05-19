3ecb4b121d9a09bfc879319c98ff099d9162dbdc
lucasmence <lucas21021996@gmail.com>
2021-05-19 09:42:42 -0300

    Added canvasBound and setPosition() to label due viewElement update; #1

--- source/system/text/label.cpp
+++ source/system/text/label.cpp
@@ -1,7 +1,14 @@
 #include "label.hpp"
 #include "../manager.hpp"
 
-Label::Label(Manager* manager, std::string caption, int size, sf::Vector2f position, int priority, sf::Color color, std::string name)
+Label::Label(Manager* manager, 
+			 std::string caption, 
+			 int size, 
+			 sf::Vector2f position, 
+			 int priority, 
+			 sf::Color color, 
+			 std::string name, 
+			 bool canvasBound)
 {
 	this->manager = manager;
     this->text = std::make_shared<sf::Text>();
@@ -10,8 +17,10 @@ Label::Label(Manager* manager, std::string caption, int size, sf::Vector2f posit
     this->text->setCharacterSize(size);
 	this->text->setString(caption);
 	this->text->setPosition(position);
+	this->position = position;
 	this->priority = priority;
 	this->name = name;
+	this->canvasBound = canvasBound;
 }
 
 Label::~Label()
@@ -21,6 +30,16 @@ Label::~Label()
 
 bool Label::draw()
 {
+	if (this->canvasBound)
+		this->text->setPosition(this->manager->canvasPosition.x + this->position.x, this->manager->canvasPosition.y + this->position.y);
 	this->manager->window->draw(*this->text);
 	return ViewElement::draw();
+}
+
+bool Label::setPosition(sf::Vector2f position)
+{
+	ViewElement::setPosition(position);
+	if (this->text)
+		this->text->setPosition(position);
+	return true;
 }
--- source/system/text/label.hpp
+++ source/system/text/label.hpp
@@ -16,9 +16,18 @@ class Label : public ViewElement
 		std::shared_ptr<sf::Text> text;
 		std::string name;
 
-		Label(Manager* manager, std::string caption, int size, sf::Vector2f position, int priority = 1, sf::Color color = sf::Color(255, 255, 255, 255), std::string name = "");
+		Label(Manager* manager, 
+			  std::string caption, 
+			  int size, 
+			  sf::Vector2f position, 
+			  int priority = 1, 
+			  sf::Color color = sf::Color(255, 255, 255, 255), 
+			  std::string name = "", 
+			  bool canvasBound = true);
+
 		~Label();
 
+		bool setPosition(sf::Vector2f position);
 		bool draw();
 };
 
