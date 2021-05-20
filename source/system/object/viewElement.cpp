cbfd92ab57696457fcaffdadb5ff92ee94baa9b1
lucasmence <lucas21021996@gmail.com>
2021-05-20 16:23:33 -0300

    Improved viewElement class by adding expiration time and auto fade off; #1

--- source/system/object/viewElement.cpp
+++ source/system/object/viewElement.cpp
@@ -3,15 +3,46 @@
 ViewElement::~ViewElement()
 {
 	this->priority = 0;
+	this->initialization();
 }
 
 bool ViewElement::draw()
 {
+	if (this->timeMax > 0.f && this->time < this->timeMax)
+	{
+		this->time += this->timeSpeed;
+		this->fading = (this->time >= this->timeMax);
+	}
 	return true;
 }
 
 bool ViewElement::setPosition(sf::Vector2f position)
 {
 	this->position = position;
+	return true;
+}
+
+sf::Vector2f ViewElement::getPosition()
+{
+	return this->position;
+}
+
+bool ViewElement::reset()
+{
+	this->visible = true;
+	this->fading = false;
+	this->time = 0.f;
+	return true;
+}
+
+bool ViewElement::initialization()
+{
+	this->visible = true;
+	this->fading = false;
+	this->timeMax = 0.f;
+	this->time = 0.f;
+	this->timeSpeed = 0.01f;
+	this->fadeSpeed = 1.f;
+
 	return true;
 }
--- source/system/object/viewElement.hpp
+++ source/system/object/viewElement.hpp
@@ -11,10 +11,20 @@ class ViewElement
 		int priority;
 		bool canvasBound;
 		sf::Vector2f position;
+		std::string name;
+		bool visible;
+		bool fading;
+		float timeMax;
+		float time;
+		float timeSpeed;
+		float fadeSpeed;
 
 		virtual ~ViewElement();
 		virtual bool draw();
 		virtual bool setPosition(sf::Vector2f position);
+		virtual sf::Vector2f getPosition();
+		virtual bool reset();
+		virtual bool initialization();
 
 };
 
