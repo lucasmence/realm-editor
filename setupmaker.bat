fa7b8c4b7c48ff6dd1e43a28c3be0b68e9de06ba
lucasmence <lucas21021996@gmail.com>
2021-05-22 16:41:37 -0300

    Added icon generation to setupmaker file; #1

--- setupmaker.bat
+++ setupmaker.bat
@@ -4,6 +4,7 @@ if not exist %smpath% mkdir %smpath%
 if not exist %smpath%\scripts mkdir %smpath%\scripts
 
 copy Debug\realm-editor.exe %smpath%\realm-editor.exe
+copy realm-editor.png %smpath%\realm-editor.png
 robocopy scripts %smpath%\scripts /E
  
 pause
