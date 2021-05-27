c58d2a24953c1cf6cab5065f3f260ac7b7a6d062
lucasmence <lucas21021996@gmail.com>
2021-05-27 10:12:29 -0300

    Updated to send the .ico file as well; #6

--- setupmaker.bat
+++ setupmaker.bat
@@ -5,6 +5,7 @@ if not exist %smpath%\scripts mkdir %smpath%\scripts
 
 copy Debug\realm-editor.exe %smpath%\realm-editor.exe
 copy realm-editor.png %smpath%\realm-editor.png
+copy realm-editor.ico %smpath%\realm-editor.ico
 robocopy scripts %smpath%\scripts /E
  
 pause
