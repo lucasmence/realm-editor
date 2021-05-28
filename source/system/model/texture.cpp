1327dde7000b54f0a4173fe022c4a948ef489002
lucasmence <lucas21021996@gmail.com>
2021-05-28 14:35:37 -0300

    Removed shared_ptr from sf::Texture; #6

--- source/system/model/texture.cpp
+++ source/system/model/texture.cpp
@@ -3,8 +3,7 @@
 Texture::Texture(std::string filename)
 {
 	this->filename = filename;
-	this->texture = std::shared_ptr<sf::Texture>(new sf::Texture);
-	this->texture->loadFromFile("resources/sprites/" + filename + ".png");
+	this->texture.loadFromFile("resources/sprites/" + filename + ".png");
 }
 
 Texture::~Texture()
