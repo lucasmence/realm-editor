167f9a309a073e63b3898b744827103285aa7089
lucasmence <lucas21021996@gmail.com>
2021-06-05 09:17:28 -0300

    Updated setupmaker script with template folder; #10

--- setupmaker.bat
+++ setupmaker.bat
@@ -4,11 +4,13 @@ if not exist %smpath% mkdir %smpath%
 if not exist %smpath%\scripts mkdir %smpath%\scripts
 if not exist %smpath%\data mkdir %smpath%\data
 if not exist %smpath%\data\options mkdir %smpath%\data\options
+if not exist %smpath%\templates mkdir %smpath%\templates
 
 copy Debug\realm-editor.exe %smpath%\realm-editor.exe
 copy realm-editor.png %smpath%\realm-editor.png
 copy realm-editor.ico %smpath%\realm-editor.ico
 copy data\options\realm-editor.json %smpath%\data\options\realm-editor.json
 robocopy scripts %smpath%\scripts /E
+robocopy templates %smpath%\templates /E
  
 pause
